ca0f68e5727e68196ae68d9b91f9b3ab39f5e98e
Corey Powell <mistdragon100@gmail.com>
2015-04-05 16:22:07 -0500

    Docs for DataCache

--- lib/bacon_bot/data_cache.rb
+++ lib/bacon_bot/data_cache.rb
@@ -4,15 +4,24 @@ module Bacon
     #   @return [String]
     attr_accessor :rootpath
 
+    # @param [String] rootpath
     def initialize(rootpath)
       @rootpath = rootpath
       @cache = {}
     end
 
+    # Loads a file from the system.
+    # Name is the basename of the file to be loaded.
+    #
+    # @param [String] name
     def load(name)
       File.read(File.join(rootpath, name))
     end
 
+    # Loads and caches a file's contents.
+    # Name is the basename of the file to be loaded.
+    #
+    # @param [String] name
     def get(name)
       @cache[name] ||= load(name)
     end
