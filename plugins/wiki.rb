17e87bd30008876df30afd9d5a071a14fd54edc7
Corey Powell <mistdragon100@gmail.com>
2015-04-05 14:32:58 -0500

    Updated wiki plugin to use storage api

--- plugins/wiki.rb
+++ plugins/wiki.rb
@@ -7,35 +7,26 @@ def cur_date
   Time.now.strftime("%D")
 end
 
-def load_fact
-  unless File.exists?("fact.yaml")
-    File.open("fact.yaml", 'w'){|f| f.write("---")}
-  end
-  YAML::load(File.read("fact.yaml"))
-end
-
-def save_fact
-  File.open("fact.yaml", "w"){|f|f.write(YAML::dump($fact))}
-end
-
-$fact = load_fact
-$fact ||= [cur_date, ""]
-save_fact
-
 plugin :Wiki do
+  def initialize(*args)
+    super
+    @fact = bbot.storage.get('fact') { [cur_date, ''] }
+    @fact.save
+  end
+
   def cmds
     ["wiki", "fact"]
   end
 
   match "fact", method: :fact
   def fact m
-    if(cur_date != $fact[0] || $fact[1].empty?)
-      $fact[0] = cur_date
-      $fact[1] = get_fact
-      save_fact
+    if (cur_date != @fact[0] || @fact[1].empty?)
+      @fact[0] = cur_date
+      @fact[1] = get_fact
+      @fact.save
     end
 
-    m.reply $fact[1]
+    m.reply @fact[1]
   end
 
   match /wiki\s+(.+)/, method: :wiki
@@ -43,7 +34,7 @@ plugin :Wiki do
     m.reply get_fact(q)
   end
 
-  def get_fact name=nil
+  def get_fact name = nil
     name ||= "Special:Random"
     url = "http://en.wikipedia.org/wiki/#{name.gsub(" ", "_")}"
 
@@ -54,7 +45,7 @@ plugin :Wiki do
     text = doc.css('div#bodyContent p')[0].text
     text.gsub!(/\[\d+\]/, "")
 
-    if(name == "Special:Random" && text.end_with?("may refer to:"))
+    if name == "Special:Random" && text.end_with?("may refer to:")
       return get_fact
     end
 
