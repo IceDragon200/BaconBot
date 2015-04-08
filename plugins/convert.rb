456d78692e70c0b45880fce7909c744d17fba4d2
Corey Powell <mistdragon100@gmail.com>
2015-04-08 18:38:58 -0500

    Updated convert plugin to handle err

--- plugins/convert.rb
+++ plugins/convert.rb
@@ -8,7 +8,13 @@ plugin :Convert do
   match /(?:con(?:vert)?) (\d+)(\w+)\s+(\w+)/, method: :con
   def con(m, i, u1, u2)
     r = http.json("http://rate-exchange.appspot.com/currency?from=#{u1}&to=#{u2}&q=#{i}")
-    j = r.data['v'].round(2)
-    m.reply "#{j}#{u2}"
+    if r.res.status != 200
+      m.reply 'Convert appears to be down.'
+    elsif r.err
+      m.reply 'ERROR: failed to parse response.'
+    else
+      j = r.data['v'].round(2)
+      m.reply "#{j}#{u2}"
+    end
   end
 end
